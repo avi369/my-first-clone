96973ecb8d77641cf5e1d3b440bcb7614fbe0282
avi bahra <avi.bahra@ecmwf.int>
2020-09-15 11:08:31 +0100

    fix

--- tests/test_useful_things.py
+++ tests/test_useful_things.py
@@ -2,7 +2,6 @@ import useful_things
 from hypothesis import given
 import hypothesis.strategies as st
 
-
 @given(st.integers(), st.integers())
 def test_add(x, y):
     assert useful_things.add(x, y) == x + y
